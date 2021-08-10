b0a6cfbd959ce5241a40bc508c89cde3813d9c42
FlorianKeisse <FlorianJoeri.Keisse@student.intecbrussel.be>
2021-08-10 09:39:35 +0200

    CustomerDAO aangepast

--- src/main/java/DAO/CustomerDAO.java
+++ src/main/java/DAO/CustomerDAO.java
@@ -5,13 +5,13 @@ import Entities.Costumer;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
-<<<<<<< HEAD
+
 import java.util.HashSet;
 import java.util.Set;
-=======
+
 import java.sql.SQLException;
 import java.util.List;
->>>>>>> 4d414ae56e624a3faba2a928ebe4a767f86fbcc4
+
 
 public class CustomerDAO {
     private EntityManagerFactory emf;
@@ -20,14 +20,13 @@ public class CustomerDAO {
         emf = EMFactory.getEMF();
     }
 
-
-<<<<<<< HEAD
     public void addCostumers(Costumer costumer) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
         em.persist(costumer);
         em.getTransaction().commit();
-=======
+    }
+
     public Costumer getCostumerByCostumerNumber(int costumerNumber) throws SQLException {
         EntityManager entityManager = emf.createEntityManager();
         return entityManager.find(Costumer.class , costumerNumber);
@@ -40,16 +39,6 @@ public class CustomerDAO {
         return countList;
     }
 
-
-
-    public void addCostumers(Costumer costumer){
-        EntityManager em = emf.createEntityManager ();
-        em.getTransaction ().begin ();
-        em.persist (costumer);
-        em.getTransaction ().commit ();
->>>>>>> 4d414ae56e624a3faba2a928ebe4a767f86fbcc4
-    }
-
     public void updateCostumers(Costumer costumer) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
@@ -57,11 +46,11 @@ public class CustomerDAO {
         em.getTransaction().commit();
     }
 
-<<<<<<< HEAD
+
     public void deleteCostumers(Costumer costumer) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-//        em.remove (em.find (Costumer.class, costumer.getId ()));
+        em.remove (em.find (Costumer.class, costumer.getCustomerNumber()));
         em.getTransaction().commit();
     }
 
@@ -74,14 +63,6 @@ public class CustomerDAO {
         EntityManager em = emf.createEntityManager();
         Query query = em.createQuery("FROM Customer c");
         return new HashSet<>(query.getResultList());
-=======
-    public void deleteCostumers(Costumer costumer){
-        EntityManager em = emf.createEntityManager ();
-        em.getTransaction ().begin ();
-        em.remove (em.find (Costumer.class, costumer.getCustomerNumber ()));
-        em.getTransaction ().commit ();
->>>>>>> 4d414ae56e624a3faba2a928ebe4a767f86fbcc4
     }
-
 
 }
