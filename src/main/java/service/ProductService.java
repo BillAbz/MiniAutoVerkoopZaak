f05e2dde5295e07d03864b5ba2be3d5075c699fd
Bilal Abz <billabz23@gmail.com>
2021-08-19 14:58:51 +0200

    product service alles done

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -5,6 +5,8 @@ import DAO.ProductDAO;
 import Entities.Employee;
 import Entities.Product;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
@@ -84,11 +86,100 @@ public class ProductService {
         return product;
     }
 
-    public void updateAProduct() {
+    public void updateAProduct() throws SQLException {
+        boolean done;
+        productDAO.getAllProducts ();
+        System.out.println ("Please enter the product code of the product you wish to update");
+        String id = scanner.nextLine ();
+        Product product = productDAO.getProductByProductCode (id);
+
+        while (product == null){
+            System.err.println ("Product doesn't exist. Please enter a valid id.");
+            id = scanner.nextLine ();
+            product = productDAO.getProductByProductCode (id);
+        }
+        System.out.println ("What would you like to update:\n");
+
+        do {
+            String updateMore;
+            System.out.println ("Please select an option: \n(1)Product name\n(2)Product line\n(3)Product scale\n(4)Product vendor\n(5)Product description\n" +
+                    "(6)Product buy price\n(7)Product MSRP\n(0)Cancel");
+            int selection = scanner.nextInt ();
+
+            while (selection > 7 || selection < 0) {
+                System.out.println ("Please make a valid selection");
+                selection = scanner.nextInt ();
+            }
+
+            switch (selection) {
+                default:
+                    break;
+                case 1:
+                    System.out.println ("Give the new name for the product name:");
+                    product.setProductName (scanner.nextLine ());
+                    System.out.println ("Done.");
+                    break;
+                case 2:
+                    System.out.println ("What is the new Product line?");
+                    product.setProductLine (scanner.nextLine ());
+                    System.out.println ("Done.");
+                    break;
+                case 3:
+                    System.out.println ("What is the new Product scale?");
+                    product.setProductScale (scanner.nextLine ());
+                    System.out.println ("Done.");
+                    break;
+                case 4:
+                    System.out.println ("What is the new Product vendor?");
+                    product.setProductVendor (scanner.nextLine ());
+                    System.out.println ("Done.");
+                    break;
+                case 5:
+                    System.out.println ("What is the new Product description?");
+                    product.setProductDescription (scanner.nextLine ());
+                    System.out.println ("Done.");
+                    break;
+                case 6:
+                    System.out.println ("What is the new Product buy price?(###.##)");
+                    product.setBuyPrice (scanner.nextBigDecimal ());
+                    System.out.println ("Done.");
+                    break;
+                case 7:
+                    System.out.println ("What is the new Product MSRP?(###.##)");
+                    product.setMSRP (scanner.nextBigDecimal ());
+                    System.out.println ("Done.");
+                    break;
+            }
+            if (selection == 0)
+                break;
+
+            System.out.println("\nWould you like to update anything else?: Y/N");
+            updateMore = scanner.next();
+            while (!updateMore.equalsIgnoreCase("y") && !updateMore.equalsIgnoreCase("n")) {
+                System.out.println("Would you like to update anything else?: ->Y/N<-");
+                updateMore = scanner.next();
+            }
+            if (updateMore.equalsIgnoreCase("n")) {
+                done = true;
+            } else done = false;
+
+        } while (!done);
+        productDAO.updateProduct (product);
 
     }
 
-    public void deleteAProduct() {
+
+    public void deleteAProduct() throws SQLException {
+        showAllProducts ();
+        System.out.println ("What product do you want delete? give the product code");
+        String id = scanner.nextLine ();
+        Product product = productDAO.getProductByProductCode (id);
+        if (product == null) {
+            System.err.println ("Product doesn't exist");
+        } else {
+            productDAO.deleteProduct (product);
+            System.out.println (product.getProductName ()+ "is deleted.");
+        }
 
     }
 
